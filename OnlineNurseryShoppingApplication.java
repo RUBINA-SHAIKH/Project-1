ff91b5dafbdfddc24312a824590c7b1970205a4d
Rubina Shaikh <123443245+RUBINA-SHAIKH@users.noreply.github.com>
2023-05-31 23:46:53 +0530

    Add files via upload

---
+++ OnlineNurseryShoppingApplication.java
@@ -0,0 +1,14 @@
+package com.onlineshopping.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineNurseryShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineNurseryShoppingApplication.class, args);
+		System.out.println("......WELCOME TO ONLINE PLANT SHOPPING......");
+	}
+
+}
